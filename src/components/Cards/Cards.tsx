161119f07240008bd2d5c26b1dbce6a3fc9e43b3
Marcos Paulo <mr.soulfox@gmail.com>
2021-07-01 14:00:08 -0300

    add scroll reveal animation

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,25 +1,41 @@
 import { Like } from '../Like/Like'
 import { Save } from '../Save/Save'
 import { formatDate } from '../../services/scripts/formatDate'
+import sr from 'scrollreveal'
+import { useEffect } from 'react'
 import './style/card.scss'
 
 export function Cards({ info }) {
     const thisCardLiked = localStorage.getItem(info.id) == null ? false : localStorage.getItem(info.id) === 'false' ? false : true 
+    
+
+
+    useEffect(() => {
+        const scroll = sr({ duration: 900, origin: 'top', distance:'50px', reset: true })
+        scroll.reveal('.notices', { delay: 200 })
+        scroll.reveal('#date', { delay: 400 })
+        scroll.reveal('#buttons', { delay: 450 })
+        scroll.reveal('#title', { delay: 500 })
+        scroll.reveal('#desc', { delay: 600 })
+
+    }, [])
+
+
 
     return (
         <div className="notices">
             <div className="notices-header">
-                <span>{formatDate(info.Date)}</span>
+                <span id="date">{formatDate(info.Date)}</span>
 
-                <div>
+                <div id="buttons">
                     <Like id={info.id} liked={thisCardLiked}/>
                     <Save id={info.id}/>
                 </div>
             </div>
 
             <div className="notices-main">
-                <span>{info.Title}</span>
-                <p>{info.Description}</p>
+                <span id="#title">{info.Title}</span>
+                <p id="#desc">{info.Description}</p>
             </div>
         </div>
     )
